8cf70bc22943df5bc2c669d247c5b7e41db692f2
Alexandr Savca <alexandr.savca89@gmail.com>
2023-03-26 18:40:06 +0300

    qemuconf.c: restyle

--- qemuconf.c
+++ qemuconf.c
@@ -39,8 +39,9 @@ int maxargc = 0;
  * Function implementations.
  */
 int
-start() {
-	if(chdir(cwd)) {
+start()
+{
+	if (chdir(cwd)) {
 		perror(cwd);
 		return 1;
 	}
@@ -49,11 +50,13 @@ start() {
 	return 1;
 }
 
-int dump() {
+int
+dump()
+{
 	int i;
 
 	fputs(cargv[0], stdout);
-	for(i = 1; i < cargc; i++) {
+	for (i = 1; i < cargc; i++) {
 		fputc(' ', stdout);
 		fputs(cargv[i], stdout);
 	}
@@ -61,15 +64,16 @@ int dump() {
 }
 
 int
-addoptarg(char *arg, int len) {
+addoptarg(char *arg, int len)
+{
 	arg[len] = '\0';
-	if(curopt != NULL) {
+	if (curopt != NULL) {
 		*curopt = arg;
 		curopt = NULL;
 		return 0;
 	}
 	cargv[cargc] = arg;
-	if(++cargc == maxargc) {
+	if (++cargc == maxargc) {
 		fputs("Too many args", stderr);
 		return 1;
 	}
@@ -79,14 +83,15 @@ addoptarg(char *arg, int len) {
 }
 
 int
-addopt(char *opt, int len) {
+addopt(char *opt, int len) 
+{
 	char *optdup;
-	if(BEGINS(opt, "cwd")) {
+	if (BEGINS(opt, "cwd"))
 		curopt = &cwd;
-	} else if(BEGINS(opt, "binary")) {
+	else if (BEGINS(opt, "binary"))
 		curopt = &binary;
-	} else {
-		if(!(optdup = calloc(sizeof(char), len + 2))) {
+	else {
+		if (!(optdup = calloc(sizeof(char), len + 2))) {
 			perror("malloc");
 			return 1;
 		}
@@ -98,28 +103,27 @@ addopt(char *opt, int len) {
 }
 
 int
-compact(char *text, int i, int len, int minindent) {
+compact(char *text, int i, int len, int minindent)
+{
 	int _i, curindent, w=i, line = 0;
 
-	for(curindent = 0, line = 0; i < len; line++, i++) {
+	for (curindent = 0, line = 0; i < len; line++, i++) {
 		DROP(isspace(text[i]) && text[i] != '\n', _i);
 		curindent = i - _i;
 
-		if(text[i] == '#' || text[i] == '\n') {
+		if (text[i] == '#' || text[i] == '\n') {
 			DROP(text[i] != '\n', i);
 			continue;
 		}
-		else if(line != 0 && curindent <= minindent) {
+		else if (line != 0 && curindent <= minindent)
 			break;
-		}
 
 		DROP(isalnum(text[i]), _i);
 		memmove(&text[w], &text[_i], i - _i);
 		w += i - _i;
 		DROP(isspace(text[i]) && text[i] != '\n', _i);
-		if(_i != i) {
+		if (_i != i)
 			text[w++] = '=';
-		}
 		DROP(text[i] != '\n', _i);
 		memmove(&text[w], &text[_i], i - _i);
 		w += i - _i;
@@ -133,28 +137,29 @@ compact(char *text, int i, int len, int minindent) {
 }
 
 int
-parseconfig(char *text, int len) {
+parseconfig(char *text, int len)
+{
 	int i = 0, _i, line, linestart, curindent;
 
-	for(linestart = i = line = 0; i < len; line++, linestart = ++i) {
+	for (linestart = i = line = 0; i < len; line++, linestart = ++i) {
 		DROP(isspace(text[i]) && text[i] != '\n', _i);
 		curindent = i - _i;
-		if(i >= len)
+		if (i >= len)
 			break;
 
-		if(text[i] == '\n')
+		if (text[i] == '\n')
 			continue;
-		if(text[i] == '#') {
+		if (text[i] == '#') {
 			DROP(text[i] != '\n', i);
 			continue;
 		}
 
-		if(text[i] == '.') {
+		if (text[i] == '.') {
 			i++;
 			DROP(isspace(text[i]), i);
 			DROP(text[i] != '\n', _i);
 			text[i] = '\0';
-			if(loadconfig(&text[_i])) {
+			if (loadconfig(&text[_i])) {
 				fprintf(stderr, "Error at line %i. ", line + 1);
 				return 1;
 			}
@@ -166,20 +171,19 @@ parseconfig(char *text, int len) {
 		addopt(&text[_i], i - _i);
 
 		DROP(isspace(text[i]) && text[i] != '\n', i);
-		if(text[i] == '\n')
+		if (text[i] == '\n')
 			continue;
-		else if(text[i] == ':' || i != _i) {
-			if(text[i] == ':') {
+		else if (text[i] == ':' || i != _i) {
+			if (text[i] == ':')
 				_i = ++i;
-			}
 			else {
 				DROP(isspace(text[i]) && text[i] != '\n', i);
 				DROP(text[i] != '\n', _i);
 			}
 
-			if(text[i-1] == ':') {
+			if (text[i-1] == ':') {
 				text[i-1] = ',';
-				if(compact(text, i, len, curindent)) {
+				if (compact(text, i, len, curindent)) {
 					fprintf(stderr, "at line %i character %i. ",
 						line + 1, i - linestart);
 					return 1;
@@ -188,8 +192,7 @@ parseconfig(char *text, int len) {
 			}
 
 			addoptarg(&text[_i], i - _i);
-		}
-		else if(i == _i) {
+		} else if (i == _i) {
 			fprintf(stderr, "Expected whitespace or ':' instead of '%c' at line %i character %i. ",
 				text[i], line + 1, i - linestart);
 			return 1;
@@ -199,25 +202,26 @@ parseconfig(char *text, int len) {
 }
 
 int
-loadconfig(char *path) {
+loadconfig(char *path)
+{
 	int r = 0, len = 0;
 	char *text = NULL, *wd;
 	char oldwd[PATH_MAX+1] = { 0 };
 	FILE *file;
 	char *pathdup = strdup(path);
 
-	if(!(file = fopen(path, "r"))) {
+	if (!(file = fopen(path, "r"))) {
 		perror(path);
 		return 1;
 	}
 
-	if(!getcwd(oldwd, sizeof(oldwd))) {
+	if (!getcwd(oldwd, sizeof(oldwd))) {
 		perror("getcwd");
 		return EXIT_FAILURE;
 	}
 
 	wd = dirname(pathdup);
-	if(chdir(wd)) {
+	if (chdir(wd)) {
 		perror(wd);
 		return 1;
 	}
@@ -225,20 +229,20 @@ loadconfig(char *path) {
 	do {
 		len += r;
 		text = realloc(text, sizeof(char) * (len + BUFSIZ));
-	} while((r = fread(text, sizeof(char), (len + BUFSIZ - 1), file)) > 0);
+	} while ((r = fread(text, sizeof(char), (len + BUFSIZ - 1), file)) > 0);
 	text[len] = 0;
 
-	if(ferror(file)) {
+	if (ferror(file)) {
 		perror(path);
 		return 1;
 	}
 
-	if(parseconfig(text, strlen(text))) {
+	if (parseconfig(text, strlen(text))) {
 		fprintf(stderr, "At file '%s'\n", path);
 		return 1;
 	}
 
-	if(chdir(oldwd)) {
+	if (chdir(oldwd)) {
 		perror(oldwd);
 		return 1;
 	}
@@ -247,12 +251,13 @@ loadconfig(char *path) {
 	return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int opt;
 	int (*action)() = start;
 
 	while ((opt = getopt(argc, argv, "nq:v")) != -1) {
-		switch(opt) {
+		switch (opt) {
 			case 'q':
 				binary = optarg;
 				break;
@@ -269,30 +274,31 @@ usage:
 				return EXIT_FAILURE;
 		}
 	}
-	if(optind >= argc)
+	if (optind >= argc)
 		goto usage;
 
-	if((maxargc = sysconf(_SC_ARG_MAX)) < 0) {
+	if ((maxargc = sysconf(_SC_ARG_MAX)) < 0) {
 		perror("sysconf");
 		return EXIT_FAILURE;
 	}
 
-	if(!(cargv = calloc(sizeof(char *), maxargc))) {
+	if (!(cargv = calloc(sizeof(char *), maxargc))) {
 		perror("calloc");
 		return EXIT_FAILURE;
 	}
 
-	if(loadconfig(argv[optind++]))
+	if (loadconfig(argv[optind++]))
 		return EXIT_FAILURE;
 
-	for(; optind < argc; optind++, cargc++) {
+	for (; optind < argc; optind++, cargc++)
 		cargv[cargc] = argv[optind];
-	}
 
-	if(!binary)
+	if (!binary)
 		binary = BINARY;
 	cargv[0] = binary;
-	if(action())
+	if (action())
 		return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }
+
+/* End of file. */
