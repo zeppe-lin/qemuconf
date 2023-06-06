a33dacc683248b06821d527ee8abce35217714f5
Alexandr Savca <alexandr.savca89@gmail.com>
2023-06-06 08:24:28 +0300

    config.mk: indent fix

--- config.mk
+++ config.mk
@@ -1,20 +1,20 @@
 # project metadata
-NAME = qemuconf
-VERSION = 0.4.2
-DIST = ${NAME}-${VERSION}
+NAME      = qemuconf
+VERSION   = 0.4.2
+DIST      = ${NAME}-${VERSION}
 
 # qemu default binary
-BINARY = qemu-system-x86_64
+BINARY    = qemu-system-x86_64
 
 # paths
 PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # flags
-CPPFLAGS = -DNAME=\"${NAME}\" -DVERSION=\"${VERSION}\" \
-	   -DBINARY=\"${BINARY}\"
-CFLAGS   = -pedantic -Wall -Wextra -Wformat
-LDFLAGS  =
+CPPFLAGS  = -DNAME=\"${NAME}\" -DVERSION=\"${VERSION}\" \
+	    -DBINARY=\"${BINARY}\"
+CFLAGS    = -pedantic -Wall -Wextra -Wformat
+LDFLAGS   =
 
 # compiler and linker
 CC = cc
