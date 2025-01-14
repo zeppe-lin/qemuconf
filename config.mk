a20b8ab120fa19a4651771c1f9123a4a0276c458
Alexandr Savca <alexandr.savca89@gmail.com>
2025-01-14 11:23:54 +0200

    config.mk: bump version from 0.4.4 to 0.4.4.1

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # project metadata
 NAME      = qemuconf
-VERSION   = 0.4.4
+VERSION   = 0.4.4.1
 DIST      = ${NAME}-${VERSION}
 
 # qemu default binary
@@ -11,7 +11,6 @@ PREFIX    = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 # flags
-CPPFLAGS  = -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\" \
-	    -DQEMU_BIN=\"${QEMU_BIN}\"
+CPPFLAGS  = -D_DEFAULT_SOURCE -DVERSION=\"${VERSION}\" -DQEMU_BIN=\"${QEMU_BIN}\"
 CFLAGS    = -pedantic -Wall -Wextra -Wformat ${CPPFLAGS}
 LDFLAGS   =
