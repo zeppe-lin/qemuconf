e9f6ca4d818536570ee17fd4548722a1a01946aa
Alexandr Savca <alexandr.savca89@gmail.com>
2025-03-23 08:00:43 +0200

    tests/run: fix for latest changes

--- tests/run.sh
+++ tests/run.sh
@@ -12,10 +12,10 @@ assert() {
 	unset -v r
 }
 
-QEMUCONF="../qemuconf -n -q q"
+QEMUCONF="../qemuconf -n"
 
 $QEMUCONF generic.conf | \
 	assert "Parse example document" "/usr/bin/qemu-system-x86_64 -smp 2 -m 1024 -enable-kvm -drive if=virtio,file=hda.img -net nic,vlan=0,model=virtio,macaddr=82:82:9D:AF:F0:1C -net user,vlan=0 -vnc 127.0.0.1:1"
 
-$QEMUCONF 00.conf | \
+$QEMUCONF -q q 00.conf | \
 	assert "Multiline argument" "q -foo bar,baz"
